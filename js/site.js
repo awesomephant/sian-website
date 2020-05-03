45e3899b3e04b5aade12170f6d7ca8bb1ec01554
Max Kohler <max@koehler-kn.de>
2020-05-03 13:23:12 +0100

    Pivot to book

--- js/site.js
+++ js/site.js
@@ -1,49 +1,32 @@
 let dishes;
 
-let state = {
-    recipeActive: true
-}
-
 function gra(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function handleDishClick() {
-    let p = this.parentElement.querySelector('.dish-recipe');
-    try {
-        document.querySelector('.dish-recipe.active').classList.remove('active')
-    } catch (e){
-
-    }
-    
-    state.recipeActive = true;
-    p.classList.add("active")
-}
-
-function handleCardClose() {
-    document.querySelector('.card.active').classList.remove("active")
-}
-
 function init() {
-    dishes = document.querySelectorAll('.dish-toggle')
-    introToggle = document.querySelector('.intro-toggle')
-    intro = document.querySelector('.site-intro')
-    // dishes[0].parentNode.querySelector('.card').classList.add('active')
-    let closeButttons = document.querySelectorAll('.card-close')
-    closeButttons.forEach((b) => {
-        b.addEventListener('click', handleCardClose)
-    })
-    dishes.forEach((d) => {
-        d.addEventListener('click', handleDishClick)
-        let x = gra(-20, 20)
-        let y = gra(-20, 20)
-        d.style.transform = `translateX(${x}%) translateY(${y}%)`
+    const book = document.querySelector('.book')
+    const rectos = document.querySelectorAll('.recto')
+    const versos = document.querySelectorAll('.verso')
+    const spreads = document.querySelectorAll('.spread')
 
+    rectos.forEach((d) => {
+        d.addEventListener('click', function (e) {
+            let p = book.getAttribute('data-currentSpread') * 1 + 1;
+            if (p < spreads.length + 1){
+                book.setAttribute('data-currentSpread', p)
+            }
+        })
+    })
+    versos.forEach((d) => {
+        d.addEventListener('click', function (e) {
+            let p = book.getAttribute('data-currentSpread') * 1 - 1;
+            if (p > 0){
+                book.setAttribute('data-currentSpread', p)
+            }
+        })
     })
 
-    introToggle.addEventListener('click', () => {
-        intro.classList.toggle('active')
-    })
 }
 window.addEventListener('DOMContentLoaded', function () {
     init()
